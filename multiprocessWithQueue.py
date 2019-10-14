30cade996cd3f3d82f74b396f8ce2c2cd7b87685
ehchoi <ehchoi@cafe24corp.com>
2019-10-14 13:44:50 +0900

    multi process with queue

--- multiprocessWithQueue.py
+++ multiprocessWithQueue.py
@@ -3,16 +3,17 @@ from os import getpid
 from time import sleep
 from random import random
 
-MAX_WORKERS = 10
+MAX_WORKERS = 5
 
-class Testing_mp(object):
+
+class TestingMp(object):
     def __init__(self):
         """
         Initiates a queue, a pool and a temporary buffer, used only
         when the queue is full.
         """
-        self.q = Queue()
-        self.pool = Pool(processes=MAX_WORKERS, initializer=self.worker_main,)
+        self.q = Queue(5000)
+        self.pool = Pool(processes=MAX_WORKERS, initializer=self.worker_main, )
         self.temp_buffer = []
 
     def add_to_queue(self, msg):
@@ -22,6 +23,7 @@ class Testing_mp(object):
         If the buffer is not empty and that the message queue is not full,
         putting back messages from the buffer to the queue.
         """
+        print("queue size : ", self.q.qsize())
         if self.q.full():
             self.temp_buffer.append(msg)
         else:
@@ -33,36 +35,33 @@ class Testing_mp(object):
         """
         This function writes some messages to the queue.
         """
-        for i in range(50):
-            self.add_to_queue("First item for loop %d" % i)
+        for i in range(1000):
+            self.add_to_queue("item for loop %d" % i)
             # Not really needed, just to show that some elements can be added
             # to the queue whenever you want!
-            sleep(random()*2)
-            self.add_to_queue("Second item for loop %d" % i)
-            # Not really needed, just to show that some elements can be added
-            # to the queue whenever you want!
-            sleep(random()*2)
+            sleep(0.1)
 
     def worker_main(self):
         """
         Waits indefinitely for an item to be written in the queue.
         Finishes when the parent process terminates.
         """
-        print ("Process {0} started".format(getpid()))
+        print("Process {0} started".format(getpid()))
         while True:
             # If queue is not empty, pop the next element and do the work.
             # If queue is empty, wait indefinitly until an element get in the queue.
             item = self.q.get(block=True, timeout=None)
-            print ("{0} retrieved: {1}".format(getpid(), item))
+            print("{0} retrieved: {1}".format(getpid(), item))
             # simulate some random length operations
-            sleep(random())
+            sleep(1)
+
 
 # Warning from Python documentation:
 # Functionality within this package requires that the __main__ module be
 # importable by the children. This means that some examples, such as the
 # multiprocessing.Pool examples will not work in the interactive interpreter.
 if __name__ == '__main__':
-    mp_class = Testing_mp()
+    mp_class = TestingMp()
     mp_class.write_to_queue()
     # Waits a bit for the child processes to do some work
     # because when the parent exits, childs are terminated.
